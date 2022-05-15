5045b9f360c93be50933e972811ce0bc8f86eb53
anselme <andesanselme@gmail.com>
2022-05-15 12:13:33 +0200

    feat: add tailwind

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwind-rn/dist';
+
+import utiilities from "./tailwind.json"
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <TailwindProvider utilities={utiilities}>
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </TailwindProvider>
   );
 }
 
