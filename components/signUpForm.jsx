68c85b505d8efe935a9e44f3ad11ad9555a8ad61
Best-Verie <irabestverie2016@gmail.com>
2022-05-17 16:52:30 +0200

    feat: sign in and sign up backend integration

--- components/signUpForm.jsx
+++ components/signUpForm.jsx
@@ -1,161 +1,245 @@
 import React from "react";
+import { useFormik } from "formik";
+
 import {
-    View,
-    StyleSheet,
-    Text,
-    TextInput,
-    Pressable,
-    TouchableOpacity,
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import react from "react";
 import { AuthContext } from "../hooks/authContext";
 
 export function SignUpForm({ navigation }) {
-    const setIsLoggedIn = react.useContext(AuthContext).setIsLoggedIn;
+  const setIsLoggedIn = react.useContext(AuthContext).setIsLoggedIn;
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.inputStyle}>
-                <Feather name="user" size={24} color="gray" />
-                <TextInput placeholder="Full Name" />
-            </View>
-            <View style={styles.inputStyle}>
-                <Feather name="phone" size={24} color="gray" />
-                <TextInput placeholder="Phone Number" />
-            </View>
-            <View style={styles.inputStyle}>
-                <EvilIcons name="envelope" size={24} color="gray" />
-                <TextInput placeholder="Your Email" />
-            </View>
+  const { handleSubmit, handleChange, handleReset, values } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      mobile: "",
+      fullName: "",
+    },
 
-            <View style={styles.submitButton}>
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => {
-                        setIsLoggedIn(true);
-                    }}
-                >
-                    <Text style={styles.cap}>Proceed</Text>
-                </TouchableOpacity>
-            </View>
+    onSubmit: async (values, { resetForm }) => {
+      if (
+        !values.email ||
+        !values.password ||
+        !values.mobile ||
+        !values.fullName
+      ) {
+        Alert.alert("Error", "You must fill in all fields");
+        return;
+      }
+      const response = await fetch(
+        "http://196.223.240.154:8099/supapp/api/auth/client/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: values.email,
+            password: values.password,
+            mobile: values.mobile,
+            firstName: values.fullName.split(" ")[0],
+            lastName: values.fullName.split(" ")[1],
+          }),
+        }
+      );
 
-            <View style={styles.optionalView}>
-                <View style={styles.leftOption}></View>
-                <Text style={styles.optionalText}>OR</Text>
-                <View style={styles.RightOption}></View>
-            </View>
+      if (!response.ok) {
+        Alert.alert("Error", "Something went wrong");
+      }
 
-            <View style={styles.pmgSignIn}>
-                <Text style={styles.pmgSignInText}>
-                    If you have a PMG account
-                </Text>
-                <View style={styles.submitButton}>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => navigation.navigate("Login")}
-                    >
-                        <Text style={styles.cap}>sign in</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                <View style={styles.bottomContent}>
-                    <Text style={styles.bottomText}>
-                        Already have an account?
-                    </Text>
-                    <Text style={styles.bottomTextTwo}>Login</Text>
-                </View>
-            </TouchableOpacity>
+      const data = await response.json();
+
+      console.log(data);
+
+      // if (!data.apierror) {
+      //   resetForm();
+      //   Alert.alert("Success", "You have successfully signed up!");
+      //   navigation.navigate("Login");
+      //   return;
+      // } else {
+      //   Alert.alert("Error", data.apierror.message);
+      // }
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputStyle}>
+        <Feather name="user" size={24} color="gray" />
+        <TextInput
+          placeholder="Full Name"
+          autoCapitalize="none"
+          value={values.fullName}
+          onChangeText={handleChange("fullName")}
+        />
+      </View>
+      <View style={styles.inputStyle}>
+        <Feather name="phone" size={24} color="gray" />
+        <TextInput
+          placeholder="Phone Number"
+          autoCapitalize="none"
+          value={values.mobile}
+          onChangeText={handleChange("mobile")}
+        />
+      </View>
+      <View style={styles.inputStyle}>
+        <EvilIcons name="envelope" size={24} color="gray" />
+        <TextInput
+          placeholder="Your Email"
+          autoCapitalize="none"
+          value={values.email}
+          onChangeText={handleChange("email")}
+        />
+      </View>
+
+      <View style={styles.inputStyle}>
+        <AntDesign name="lock" size={24} color="gray" />
+        <TextInput
+          placeholder="Your Password"
+          autoCapitalize="none"
+          value={values.password}
+          secureTextEntry={true}
+          onChangeText={handleChange("password")}
+        />
+      </View>
+
+      <View style={styles.submitButton}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            // setIsLoggedIn(true);
+            handleSubmit();
+          }}
+        >
+          <Text style={styles.cap}>Proceed</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.optionalView}>
+        <View style={styles.leftOption}></View>
+        <Text style={styles.optionalText}>OR</Text>
+        <View style={styles.RightOption}></View>
+      </View>
+
+      <View style={styles.pmgSignIn}>
+        <Text style={styles.pmgSignInText}>If you have a PMG account</Text>
+        <View style={styles.submitButton}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <Text style={styles.cap}>Sign in</Text>
+          </TouchableOpacity>
         </View>
-    );
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <View style={styles.bottomContent}>
+          <Text style={styles.bottomText}>Already have an account?</Text>
+          <Text style={styles.bottomTextTwo}>Login</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-    },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
 
-    inputStyle: {
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 10,
-        marginTop: 10,
-        width: 327,
-        height: 54,
-        borderRadius: 8,
-        borderColor: "#EAECEE",
-        borderWidth: 2,
-        paddingLeft: 10,
-    },
-    submitButton: {
-        marginTop: 20,
-    },
+  inputStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    marginTop: 10,
+    width: 327,
+    height: 54,
+    borderRadius: 8,
+    borderColor: "#EAECEE",
+    borderWidth: 2,
+    paddingLeft: 10,
+  },
+  submitButton: {
+    marginTop: 20,
+  },
 
-    button: {
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 15,
-        backgroundColor: "#F7941D",
-        width: 327,
-        height: 50,
-    },
-    cap: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: "bold",
-        letterSpacing: 0.25,
-        color: "white",
-    },
-    optionalView: {
-        marginTop: 20,
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-    leftOption: {
-        width: "30%",
-        height: 1,
-        backgroundColor: "gray",
-    },
-    RightOption: {
-        width: "30%",
-        height: 1,
-        backgroundColor: "gray",
-    },
-    optionalText: {
-        fontSize: 16,
-        color: "gray",
-        fontWeight: "bold",
-    },
-    pmgSignIn: {
-        marginTop: 10,
-    },
-    pmgSignInText: {
-        fontSize: 16,
-        color: "gray",
-        textAlign: "center",
-    },
-    bottomContent: {
-        marginTop: 20,
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 20,
-    },
-    bottomText: {
-        fontSize: 16,
-        color: "gray",
-        textAlign: "center",
-    },
-    bottomTextTwo: {
-        fontSize: 16,
-        color: "#F7941D",
-        textAlign: "center",
-    },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15,
+    backgroundColor: "#F7941D",
+    width: 327,
+    height: 50,
+    shadowColor: "rgba(0, 0, 0, 0.1)",
+    shadowOpacity: 0.1,
+    elevation: 1,
+    shadowRadius: 15,
+    shadowOffset: { width: 1, height: 13 },
+  },
+  cap: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  optionalView: {
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  leftOption: {
+    width: "30%",
+    height: 1,
+    backgroundColor: "gray",
+  },
+  RightOption: {
+    width: "30%",
+    height: 1,
+    backgroundColor: "gray",
+  },
+  optionalText: {
+    fontSize: 16,
+    color: "gray",
+    fontWeight: "bold",
+  },
+  pmgSignIn: {
+    marginTop: 10,
+  },
+  pmgSignInText: {
+    fontSize: 16,
+    color: "gray",
+    textAlign: "center",
+  },
+  bottomContent: {
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  bottomText: {
+    fontSize: 16,
+    color: "gray",
+    textAlign: "center",
+  },
+  bottomTextTwo: {
+    fontSize: 16,
+    color: "#F7941D",
+    textAlign: "center",
+  },
 });
--- screens/app/checkout.js
+++ screens/app/checkout.js
@@ -1,286 +1,286 @@
 import {
-    SafeAreaView,
-    Pressable,
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    ScrollView,
+  SafeAreaView,
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
 } from "react-native";
 import { FontAwesome, Ionicons, EvilIcons } from "@expo/vector-icons";
 import Screen from "../../layouts/Screen";
 
 export default function CheckoutScreen({ navigation }) {
-    return (
-        <Screen>
-            <SafeAreaView style={styles.mainView}>
-                <View style={styles.container}>
-                    <Pressable>
-                        <Ionicons
-                            style={styles.back}
-                            name="chevron-back-outline"
-                            size={60}
-                            color="green"
-                        />
-                    </Pressable>
+  return (
+    <Screen>
+      <SafeAreaView style={styles.mainView}>
+        <View style={styles.container}>
+          <Pressable>
+            <Ionicons
+              style={styles.back}
+              name="chevron-back-outline"
+              size={60}
+              color="green"
+            />
+          </Pressable>
 
-                    <View style={styles.checkoutHeader}>
-                        <Text style={styles.title}>Checkout</Text>
-                        <FontAwesome
-                            style={styles.creditCard}
-                            name="credit-card"
-                            size={18}
-                            color="black"
-                        />
-                    </View>
+          <View style={styles.checkoutHeader}>
+            <Text style={styles.title}>Checkout</Text>
+            <FontAwesome
+              style={styles.creditCard}
+              name="credit-card"
+              size={18}
+              color="black"
+            />
+          </View>
 
-                    <View>
-                        <Text style={styles.price}>Frw 16,000</Text>
-                        <Text style={styles.tax}>Including VAT(18%)</Text>
-                    </View>
+          <View>
+            <Text style={styles.price}>Frw 16,000</Text>
+            <Text style={styles.tax}>Including VAT(18%)</Text>
+          </View>
 
-                    <Pressable>
-                        <View style={{ flexDirection: "row", width: "100%" }}>
-                            <View style={[styles.cardContainer1]}>
-                                <Text style={styles.card}>Credit card</Text>
-                            </View>
+          <Pressable>
+            <View style={{ flexDirection: "row", width: "100%" }}>
+              <View style={[styles.cardContainer1]}>
+                <Text style={styles.card}>Credit card</Text>
+              </View>
 
-                            <View style={styles.cardContainer2}>
-                                <Text style={styles.cash}>Mobile & Cash</Text>
-                            </View>
-                        </View>
-                    </Pressable>
-                </View>
+              <View style={styles.cardContainer2}>
+                <Text style={styles.cash}>Mobile & Cash</Text>
+              </View>
+            </View>
+          </Pressable>
+        </View>
 
-                <View style={styles.containerMtn}>
-                    <Image
-                        style={styles.containerImage}
-                        source={require("../../assets/mtn.png")}
-                    />
-                    <View style={styles.top}>
-                        <Text style={styles.heading}>MTN Mobile Money</Text>
-                    </View>
-                </View>
+        <View style={styles.containerMtn}>
+          <Image
+            style={styles.containerImage}
+            source={require("../../assets/mtn.png")}
+          />
+          <View style={styles.top}>
+            <Text style={styles.heading}>MTN Mobile Money</Text>
+          </View>
+        </View>
 
-                <View style={styles.containerAirTel}>
-                    <Image
-                        style={styles.containerImage}
-                        source={require("../../assets/air.png")}
-                    />
-                    <View style={styles.top}>
-                        <Text style={styles.heading}>Airtel Money</Text>
-                    </View>
-                </View>
+        <View style={styles.containerAirTel}>
+          <Image
+            style={styles.containerImage}
+            source={require("../../assets/air.png")}
+          />
+          <View style={styles.top}>
+            <Text style={styles.heading}>Airtel Money</Text>
+          </View>
+        </View>
 
-                <View style={styles.containerCash}>
-                    <Image
-                        style={styles.containerImage}
-                        source={require("../../assets/cash.png")}
-                    />
-                    <View style={styles.top}>
-                        <Text style={styles.heading}>Cash</Text>
-                    </View>
-                </View>
+        <View style={styles.containerCash}>
+          <Image
+            style={styles.containerImage}
+            source={require("../../assets/cash.png")}
+          />
+          <View style={styles.top}>
+            <Text style={styles.heading}>Cash</Text>
+          </View>
+        </View>
 
-                <View>
-                    <Text style={styles.paragraph}>
-                        We will send you an order details to your {"\n"} email
-                        after the successful payment
-                    </Text>
-                </View>
+        <View>
+          <Text style={styles.paragraph}>
+            We will send you an order details to your {"\n"} email after the
+            successful payment
+          </Text>
+        </View>
 
-                <Pressable
-                    onPress={() => {
-                        navigation.navigate("Success");
-                    }}
-                >
-                    <View style={styles.buttonAndIcon}>
-                        <EvilIcons
-                            style={styles.icon}
-                            name="unlock"
-                            size={50}
-                            color="white"
-                        />
-                        <Text style={styles.buttonText}>Pay for the order</Text>
-                    </View>
-                </Pressable>
-            </SafeAreaView>
-        </Screen>
-    );
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Success");
+          }}
+        >
+          <View style={styles.buttonAndIcon}>
+            <EvilIcons
+              style={styles.icon}
+              name="unlock"
+              size={50}
+              color="white"
+            />
+            <Text style={styles.buttonText}>Pay for the order</Text>
+          </View>
+        </Pressable>
+      </SafeAreaView>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    mainView: {
-        marginTop: -18,
-        width: "100%",
-        minHeight: "100%",
-        paddingBottom: 100,
-        backgroundColor: "#ffffff",
-        zIndex: 2,
-    },
-    container: {
-        backgroundColor: "#ffffff",
-        height: 280,
-        width: "96%",
-        marginBottom: 100,
-        marginTop: 0,
-        borderRadius: 30,
-        borderWidth: 0,
-        elevation: 30,
-    },
-    price: {
-        color: "#25D482",
-        textAlign: "right",
-        fontSize: 20,
-        marginRight: 10,
-        marginTop: -30,
-    },
-    tax: {
-        color: "#000000",
-        fontSize: 14,
-        textAlign: "right",
-        marginRight: 10,
-    },
-    buttons: {
-        display: "inline",
-    },
+  mainView: {
+    marginTop: -18,
+    width: "100%",
+    minHeight: "100%",
+    paddingBottom: 100,
+    backgroundColor: "#ffffff",
+    zIndex: 2,
+  },
+  container: {
+    backgroundColor: "#ffffff",
+    height: 280,
+    width: "96%",
+    marginBottom: 100,
+    marginTop: 0,
+    borderRadius: 30,
+    borderWidth: 0,
+    elevation: 30,
+  },
+  price: {
+    color: "#25D482",
+    textAlign: "right",
+    fontSize: 20,
+    marginRight: 10,
+    marginTop: -30,
+  },
+  tax: {
+    color: "#000000",
+    fontSize: 14,
+    textAlign: "right",
+    marginRight: 10,
+  },
+  buttons: {
+    display: "inline",
+  },
 
-    btn1: {
-        color: "#000000",
-        backgroundColor: "#ffffff",
-        marginLeft: 10,
-    },
-    btn2: {
-        color: "#ffffff",
-        backgroundColor: "#0CC805",
-        marginLeft: 60,
-    },
-    heading: {
-        color: "#000",
-        fontSize: 20,
-        marginLeft: 50,
-        marginTop: 50,
-    },
-    containerImage: {
-        width: 150,
-        height: 140,
-        marginLeft: 1,
-        marginTop: 10,
-    },
-    top: {
-        marginTop: 0,
-    },
+  btn1: {
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    marginLeft: 10,
+  },
+  btn2: {
+    color: "#ffffff",
+    backgroundColor: "#0CC805",
+    marginLeft: 60,
+  },
+  heading: {
+    color: "#000",
+    fontSize: 20,
+    marginLeft: 50,
+    marginTop: 50,
+  },
+  containerImage: {
+    width: 150,
+    height: 140,
+    marginLeft: 1,
+    marginTop: 10,
+  },
+  top: {
+    marginTop: 0,
+  },
 
-    containerMtn: {
-        display: "flex",
-        flexDirection: "row",
-        width: "90%",
-        height: 80,
-        marginLeft: 20,
-        marginBottom: 60,
-    },
-    containerAirTel: {
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: "rgba(242,242,242,1)",
-        width: "90%",
-        height: 130,
-        marginLeft: 20,
-        marginBottom: 10,
-    },
-    containerCash: {
-        display: "flex",
-        flexDirection: "row",
-        width: "90%",
-        height: 80,
-        marginLeft: 20,
-        marginBottom: 140,
-    },
+  containerMtn: {
+    display: "flex",
+    flexDirection: "row",
+    width: "90%",
+    height: 80,
+    marginLeft: 20,
+    marginBottom: 60,
+  },
+  containerAirTel: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "rgba(242,242,242,1)",
+    width: "90%",
+    height: 130,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  containerCash: {
+    display: "flex",
+    flexDirection: "row",
+    width: "90%",
+    height: 80,
+    marginLeft: 20,
+    marginBottom: 140,
+  },
 
-    paragraph: {
-        fontSize: 20,
-        marginLeft: 20,
-        color: "#000",
-    },
-    buttonAndIcon: {
-        backgroundColor: "#25D482",
-        width: "80%",
-        height: 70,
-        marginLeft: "10%",
-        marginTop: 30,
-        borderRadius: 30,
-        display: "flex",
-        flexDirection: "row",
-        textAlign: "center",
-        alignItems: "center",
-    },
-    icon: {
-        marginLeft: 70,
-        alignItems: "center",
-    },
-    buttonText: {
-        color: "rgb(255,255,255)",
-        fontSize: 24,
-        marginLeft: 10,
-    },
-    checkoutHeader: {
-        display: "flex",
-        flexDirection: "row",
-        marginTop: 50,
-        fontSize: 0,
-    },
-    title: {
-        color: "#000",
-        bold: "700",
-        fontSize: 22,
-        marginLeft: 40,
-    },
-    creditCard: {
-        marginLeft: 12,
-        marginTop: 10,
-        backgroundColor: "gold",
-        height: 18,
-    },
+  paragraph: {
+    fontSize: 20,
+    marginLeft: 20,
+    color: "#000",
+  },
+  buttonAndIcon: {
+    backgroundColor: "#25D482",
+    width: "80%",
+    height: 70,
+    marginLeft: "10%",
+    marginTop: 30,
+    borderRadius: 30,
+    display: "flex",
+    flexDirection: "row",
+    textAlign: "center",
+    alignItems: "center",
+  },
+  icon: {
+    marginLeft: 70,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "rgb(255,255,255)",
+    fontSize: 24,
+    marginLeft: 10,
+  },
+  checkoutHeader: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 50,
+    fontSize: 0,
+  },
+  title: {
+    color: "#000",
+    fontWeight: "700",
+    fontSize: 22,
+    marginLeft: 40,
+  },
+  creditCard: {
+    marginLeft: 12,
+    marginTop: 10,
+    backgroundColor: "gold",
+    height: 18,
+  },
 
-    card: {
-        backgroundColor: "#ffffff",
-        fontSize: 20,
-        color: "#000",
-    },
-    cash: {
-        color: "#ffffff",
-        textAlign: "center",
-        fontSize: 20,
-        backgroundColor: "#25D482",
-    },
+  card: {
+    backgroundColor: "#ffffff",
+    fontSize: 20,
+    color: "#000",
+  },
+  cash: {
+    color: "#ffffff",
+    textAlign: "center",
+    fontSize: 20,
+    backgroundColor: "#25D482",
+  },
 
-    cardContainer1: {
-        minHeight: 40,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#ffffff",
-        marginTop: 50,
-        marginLeft: 40,
-        width: 170,
-        borderRadius: 20,
-        borderWidth: 0,
-        elevation: 10,
-    },
-    cardContainer2: {
-        minHeight: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 50,
-        height: 70,
-        width: 170,
-        marginLeft: -18,
-        backgroundColor: "#25D489",
-        borderRadius: 20,
-    },
-    back: {
-        marginTop: 48,
-        marginLeft: 20,
-        backgroundColor: "rgba(242,242,242,1)",
-        width: 60,
-    },
+  cardContainer1: {
+    minHeight: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffff",
+    marginTop: 50,
+    marginLeft: 40,
+    width: 170,
+    borderRadius: 20,
+    borderWidth: 0,
+    elevation: 10,
+  },
+  cardContainer2: {
+    minHeight: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+    height: 70,
+    width: 170,
+    marginLeft: -18,
+    backgroundColor: "#25D489",
+    borderRadius: 20,
+  },
+  back: {
+    marginTop: 48,
+    marginLeft: 20,
+    backgroundColor: "rgba(242,242,242,1)",
+    width: 60,
+  },
 });
