0b928c2ae486981735bb81b6b72be47119c97940
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2022-06-11 10:28:11 +0200

    fix: the issue on login

--- components/signInForm.jsx
+++ components/signInForm.jsx
@@ -1,11 +1,11 @@
 import react from "react";
 import {
-  View,
-  StyleSheet,
-  Text,
-  TextInput,
-  Alert,
-  TouchableOpacity,
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    Alert,
+    TouchableOpacity,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons";
@@ -15,248 +15,253 @@ import { useFormik } from "formik";
 import * as SecureStore from "expo-secure-store";
 
 export function SignInForm(props) {
-  const { navigation } = props;
+    const { navigation } = props;
 
-  const setIsLoggedIn = react.useContext(AuthContext).setIsLoggedIn;
+    const setIsLoggedIn = react.useContext(AuthContext).setIsLoggedIn;
 
-  const { handleSubmit, handleChange, handleReset, values } = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: async (values, { resetForm }) => {
-      if (!values.email || !values.password) {
-        Alert.alert("Error", "You must fill in all fields");
-        return;
-      }
-      const response = await fetch(
-        "http://196.223.240.154:8099/supapp/api/auth/signin",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            login: values.email,
-            password: values.password,
-          }),
-        }
-      );
+    const { handleSubmit, handleChange, handleReset, values } = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: async (values, { resetForm }) => {
+            if (!values.email || !values.password) {
+                Alert.alert("Error", "You must fill in all fields");
+                return;
+            }
 
-      if (!response.ok) {
-        Alert.alert("Error", "Invalid credentials");
-      }
+            const response = await fetch(
+                "http://196.223.240.154:8099/supapp/api/auth/signin",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        login: values.email,
+                        password: values.password,
+                    }),
+                }
+            );
 
-      const data = await response.json();
-      Alert.alert("Success", "You have successfully signed in");
+            if (!response.ok) {
+                Alert.alert("Error", "Invalid credentials");
 
-      if (data.token.accessToken) {
-        await SecureStore.setItemAsync("token", data.token.accessToken);
-        setIsLoggedIn(true);
-      }
-    },
-  });
+                return;
+            }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputStyle}>
-        <EvilIcons name="envelope" size={24} color="gray" />
-        <TextInput
-          placeholder="Your Email"
-          value={values.email}
-          onChangeText={handleChange("email")}
-        />
-      </View>
-      <View style={styles.inputStyle}>
-        <AntDesign name="lock" size={24} color="gray" />
-        <TextInput
-          placeholder="Password"
-          value={values.password}
-          onChangeText={handleChange("password")}
-        />
-      </View>
-      <View style={styles.submitButton}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            handleSubmit();
-          }}
-        >
-          <Text style={styles.cap}>Sign In</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.optionalView}>
-        <View style={styles.leftOption}></View>
-        <Text style={styles.optionalText}>OR</Text>
-        <View style={styles.RightOption}></View>
-      </View>
-      <View style={styles.gButton}>
-        <FontAwesome name="google" size={24} color="black" />
-        <Text style={styles.gCap}>Login with google</Text>
-      </View>
-      <View style={styles.fbButton}>
-        <FontAwesome name="facebook" size={24} color="black" />
-        <Text style={styles.fCap}>Login with facebook</Text>
-      </View>
+            const data = await response.json();
+            Alert.alert("Success", "You have successfully signed in");
 
-      <View style={styles.fgtPass}>
-        <Text style={styles.fgtPassText}>Forgot Password</Text>
-      </View>
-      <TouchableOpacity
-        onPress={() => {
-          console.log("Go to register");
-          navigation.navigate("Register");
-        }}
-      >
-        <View style={styles.bottomContent}>
-          <Text style={styles.bottomText}>Don't have an account?</Text>
-          <Text style={styles.bottomTextTwo}>Register</Text>
+            if (data.token.accessToken) {
+                await SecureStore.setItemAsync("token", data.token.accessToken);
+                setIsLoggedIn(true);
+            }
+        },
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputStyle}>
+                <EvilIcons name="envelope" size={24} color="gray" />
+                <TextInput
+                    placeholder="Your Email"
+                    value={values.email}
+                    onChangeText={handleChange("email")}
+                />
+            </View>
+            <View style={styles.inputStyle}>
+                <AntDesign name="lock" size={24} color="gray" />
+                <TextInput
+                    placeholder="Password"
+                    value={values.password}
+                    onChangeText={handleChange("password")}
+                />
+            </View>
+            <View style={styles.submitButton}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        handleSubmit();
+                    }}
+                >
+                    <Text style={styles.cap}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.optionalView}>
+                <View style={styles.leftOption}></View>
+                <Text style={styles.optionalText}>OR</Text>
+                <View style={styles.RightOption}></View>
+            </View>
+            <View style={styles.gButton}>
+                <FontAwesome name="google" size={24} color="black" />
+                <Text style={styles.gCap}>Login with google</Text>
+            </View>
+            <View style={styles.fbButton}>
+                <FontAwesome name="facebook" size={24} color="black" />
+                <Text style={styles.fCap}>Login with facebook</Text>
+            </View>
+
+            <View style={styles.fgtPass}>
+                <Text style={styles.fgtPassText}>Forgot Password</Text>
+            </View>
+            <TouchableOpacity
+                onPress={() => {
+                    console.log("Go to register");
+                    navigation.navigate("Register");
+                }}
+            >
+                <View style={styles.bottomContent}>
+                    <Text style={styles.bottomText}>
+                        Don't have an account?
+                    </Text>
+                    <Text style={styles.bottomTextTwo}>Register</Text>
+                </View>
+            </TouchableOpacity>
         </View>
-      </TouchableOpacity>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    // height: "100%",
-  },
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fff",
+        // height: "100%",
+    },
 
-  welcomeText: {
-    color: "#3A4874",
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  signIntext: {
-    textAlign: "center",
-    color: "gray",
-  },
-  inputStyle: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10,
-    marginTop: 30,
-    width: 327,
-    height: 54,
-    borderRadius: 8,
-    borderColor: "#EAECEE",
-    borderWidth: 2,
-    paddingLeft: 10,
-  },
-  submitButton: {
-    marginTop: 20,
-  },
+    welcomeText: {
+        color: "#3A4874",
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    signIntext: {
+        textAlign: "center",
+        color: "gray",
+    },
+    inputStyle: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+        marginTop: 30,
+        width: 327,
+        height: 54,
+        borderRadius: 8,
+        borderColor: "#EAECEE",
+        borderWidth: 2,
+        paddingLeft: 10,
+    },
+    submitButton: {
+        marginTop: 20,
+    },
 
-  button: {
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 15,
-    backgroundColor: "#F7941D",
-    width: 327,
-    height: 50,
-    shadowColor: "rgba(0, 0, 0, 0.1)",
-    shadowOpacity: 0.1,
-    elevation: 1,
-    shadowRadius: 15,
-    shadowOffset: { width: 1, height: 13 },
-  },
-  cap: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
-  },
-  optionalView: {
-    marginTop: 10,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  leftOption: {
-    width: "30%",
-    height: 1,
-    backgroundColor: "gray",
-  },
-  RightOption: {
-    width: "30%",
-    height: 1,
-    backgroundColor: "gray",
-  },
-  optionalText: {
-    fontSize: 16,
-    color: "gray",
-    fontWeight: "bold",
-  },
-  pmgSignIn: {
-    marginTop: 10,
-  },
-  pmgSignInText: {
-    fontSize: 16,
-    color: "gray",
-    textAlign: "center",
-  },
-  bottomContent: {
-    marginTop: 10,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 10,
-  },
-  bottomText: {
-    fontSize: 16,
-    color: "gray",
-    textAlign: "center",
-  },
-  bottomTextTwo: {
-    fontSize: 16,
-    color: "#F7941D",
-    textAlign: "center",
-  },
-  gButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 10,
-    alignItems: "center",
-    padding: 15,
-    width: 327,
-    height: 60,
-    borderRadius: 8,
-    Color: "gray",
-    borderRadius: 8,
-    borderColor: "#EAECEE",
-    borderWidth: 2,
-  },
-  fbButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 10,
-    alignItems: "center",
-    padding: 15,
-    width: 327,
-    height: 60,
-    borderRadius: 8,
-    borderColor: "#EAECEE",
-    borderWidth: 2,
-  },
-  gCap: {
-    marginLeft: 10,
-    color: "gray",
-  },
-  fCap: {
-    marginLeft: 10,
-    color: "gray",
-  },
-  fgtPass: {
-    marginTop: 10,
-  },
-  fgtPassText: {
-    fontSize: 16,
-    color: "#F7941D",
-  },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 15,
+        backgroundColor: "#F7941D",
+        width: 327,
+        height: 50,
+        shadowColor: "rgba(0, 0, 0, 0.1)",
+        shadowOpacity: 0.1,
+        elevation: 1,
+        shadowRadius: 15,
+        shadowOffset: { width: 1, height: 13 },
+    },
+    cap: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: "bold",
+        letterSpacing: 0.25,
+        color: "white",
+    },
+    optionalView: {
+        marginTop: 10,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    leftOption: {
+        width: "30%",
+        height: 1,
+        backgroundColor: "gray",
+    },
+    RightOption: {
+        width: "30%",
+        height: 1,
+        backgroundColor: "gray",
+    },
+    optionalText: {
+        fontSize: 16,
+        color: "gray",
+        fontWeight: "bold",
+    },
+    pmgSignIn: {
+        marginTop: 10,
+    },
+    pmgSignInText: {
+        fontSize: 16,
+        color: "gray",
+        textAlign: "center",
+    },
+    bottomContent: {
+        marginTop: 10,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+    bottomText: {
+        fontSize: 16,
+        color: "gray",
+        textAlign: "center",
+    },
+    bottomTextTwo: {
+        fontSize: 16,
+        color: "#F7941D",
+        textAlign: "center",
+    },
+    gButton: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 10,
+        alignItems: "center",
+        padding: 15,
+        width: 327,
+        height: 60,
+        borderRadius: 8,
+        Color: "gray",
+        borderRadius: 8,
+        borderColor: "#EAECEE",
+        borderWidth: 2,
+    },
+    fbButton: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 10,
+        alignItems: "center",
+        padding: 15,
+        width: 327,
+        height: 60,
+        borderRadius: 8,
+        borderColor: "#EAECEE",
+        borderWidth: 2,
+    },
+    gCap: {
+        marginLeft: 10,
+        color: "gray",
+    },
+    fCap: {
+        marginLeft: 10,
+        color: "gray",
+    },
+    fgtPass: {
+        marginTop: 10,
+    },
+    fgtPassText: {
+        fontSize: 16,
+        color: "#F7941D",
+    },
 });
--- screens/auth/register.js
+++ screens/auth/register.js
@@ -3,77 +3,77 @@ import { SignUpForm } from "../../components/signUpForm";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RegisterScreen({ navigation }) {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={styles.contentView}>
-            <View style={styles.introView}>
-              <View style={styles.logo}>
-                <Text style={styles.logoText}>Supa</Text>
-                <Text style={styles.logoTextM}>Menu</Text>
-              </View>
-              <Text style={styles.welcomeText}>Welcome ...</Text>
-              <Text style={styles.signIntext}>
-                Please fill in the information
-              </Text>
-            </View>
-            <View style={styles.loginForm}>
-              <SignUpForm navigation={navigation} />
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.contentView}>
+                        <View style={styles.introView}>
+                            <View style={styles.logo}>
+                                <Text style={styles.logoText}>Supa</Text>
+                                <Text style={styles.logoTextM}>Menu</Text>
+                            </View>
+                            <Text style={styles.welcomeText}>Welcome ...</Text>
+                            <Text style={styles.signIntext}>
+                                Please fill in the information
+                            </Text>
+                        </View>
+                        <View style={styles.loginForm}>
+                            <SignUpForm navigation={navigation} />
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F7941D",
-  },
-  contentView: {
-    marginTop: 100,
-    backgroundColor: "#fff",
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    alignContent: "center",
-    justifyContent: "flex-start",
-  },
-  introView: {
-    marginBottom: 20,
-  },
-  logo: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logoText: {
-    fontSize: 40,
-    fontWeight: "bold",
-  },
-  logoTextM: {
-    fontSize: 40,
-    fontWeight: "bold",
-    marginLeft: 3,
-    color: "#F7941D",
-  },
-  welcomeText: {
-    color: "#3A4874",
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  signIntext: {
-    textAlign: "center",
-    color: "gray",
-  },
-  loginForm: {
-    marginTop: 20,
-    justifyContent: "flex-end",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#F7941D",
+    },
+    contentView: {
+        marginTop: 100,
+        backgroundColor: "#fff",
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        alignContent: "center",
+        justifyContent: "flex-start",
+    },
+    introView: {
+        marginBottom: 20,
+    },
+    logo: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    logoText: {
+        fontSize: 40,
+        fontWeight: "bold",
+    },
+    logoTextM: {
+        fontSize: 40,
+        fontWeight: "bold",
+        marginLeft: 3,
+        color: "#F7941D",
+    },
+    welcomeText: {
+        color: "#3A4874",
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    signIntext: {
+        textAlign: "center",
+        color: "gray",
+    },
+    loginForm: {
+        marginTop: 20,
+        justifyContent: "flex-end",
+    },
 });
